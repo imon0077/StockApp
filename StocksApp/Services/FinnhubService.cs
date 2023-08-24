b87dda1a5887d64e63badc6e5fac45127a60b319
Imon Islam <imon0077@yahoo.com>
2023-08-24 20:21:11 +0600

    Finnhub service & contracts added to get the price quote from finnhub.io api, also gettign it in controller.

---
+++ StocksApp/Services/FinnhubService.cs
@@ -0,0 +1,45 @@
+﻿using StocksApp.ServiceContracts;
+using System.Text.Json;
+
+namespace StocksApp.Services
+{
+    public class FinnhubService : IFinnhubService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FinnhubService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
+        {
+            using (HttpClient httpClient = _httpClientFactory.CreateClient())
+            {
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri($"https://finnhub.io/api/v1/quote?symbol={stockSymbol}&token=cjjcv0hr01qirue67d20cjjcv0hr01qirue67d2g"),
+                    Method = HttpMethod.Get
+                };
+
+                HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+                Stream stream = httpResponseMessage.Content.ReadAsStream();
+                
+                StreamReader streamReader = new StreamReader(stream);
+
+                string response = streamReader.ReadToEnd();
+
+                Dictionary<string, object>? responseDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(response);
+
+                if (responseDictionary == null)
+                    throw new InvalidOperationException("No response form finnhub service");
+                if (responseDictionary.ContainsKey("error"))
+                    throw new InvalidOperationException(Convert.ToString(responseDictionary["error"]));
+
+                return responseDictionary;
+            }
+
+        }
+    }
+}
