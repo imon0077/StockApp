8386cc567288686c8982318ee89ffb326744527b
Imon Islam <imon0077@yahoo.com>
2023-08-24 10:22:32 +0600

    initial commit

---
+++ StocksApp/Program.cs
@@ -0,0 +1,15 @@
+using StocksApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<MyService>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+
+app.Run();
